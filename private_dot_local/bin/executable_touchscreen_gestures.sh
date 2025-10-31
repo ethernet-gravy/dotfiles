c1af67599a80c2d0f6e41ac60c0f0438025c306f
Nakul S. <nakulsodhi@protonmail.com>
2025-10-31 08:14:18 -0500

    I think i have finally fixed the touchscreen gestures not running in niri, hopefully

--- private_dot_local/bin/executable_touchscreen_gestures.sh
+++ private_dot_local/bin/executable_touchscreen_gestures.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-lisgd -d /dev/input/by-id/usb-Wacom_Co._Ltd._Pen_and_multitouch_sensor-event-if00 -t 20 -T 10 -g "1,RL,R,*,R,niri msg action focus-column-right" -g "1,LR,L,*,R,niri msg action focus-column-left" -g "1,DU,B,*,R,niri msg action open-overview" >> ~/output.txt
+lisgd -d /dev/input/by-id/usb-Wacom_Co._Ltd._Pen_and_multitouch_sensor-event-if00 -t 10 -T 5 -r 30 -g "1,RL,R,*,R,niri msg action focus-column-right" -g "1,LR,L,*,R,niri msg action focus-column-left" -g "1,DU,B,*,R,niri msg action open-overview" >> ~/output.txt
