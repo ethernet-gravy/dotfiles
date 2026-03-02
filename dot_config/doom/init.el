21c9afce23a1f15775580822416e06c1737ee47e
Nakul S. <nakulsodhi@protonmail.com>
2026-03-02 16:13:57 -0600

    added org appear back

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -155,7 +155,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +noter +dragndrop) ;; +pretty)
+       (org +roam2 +noter +dragndrop +pretty) ;; +pretty)
                                         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
